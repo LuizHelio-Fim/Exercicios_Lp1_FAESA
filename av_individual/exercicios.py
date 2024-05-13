3342a50755de642f7b5d4d319d88d24e62e482da
Luiz Hélio <luizhelionerd@hotmail.com>
2024-05-13 12:17:15 -0300

    Create exercicios.py

---
+++ av_individual/exercicios.py
@@ -0,0 +1,41 @@
+#1
+
+xpto = 3
+
+if xpto > 3 or xpto == 3:
+    xpto *= -1
+else:
+    xpto = 0
+
+print(xpto)
+
+#2
+
+a = 10
+
+if a < 15 or a != 10:
+    print(1)
+elif a == 10:
+    print(2)
+else:
+    print(3)
+
+#5
+
+for i in range(-5, 8, 3):
+    print(i)
+
+#6
+
+for j in range(10, 0, -1):
+    print(j)
+
+#8
+
+for k in range(3):
+    print(k)
+
+#9
+
+for l in range(33):
+    print(l)
